cf925bd2ad6bd8939a0e7c549f4241564e3b775b
TY-A-02-173\akira.sugisaki <akira.sugisaki@linkedbrain.jp>
2025-05-09 02:41:02 +0900

    fix ヒエラルキーの構成修正、プレイヤーの移動方向をカメラの向きに合わせる

--- JamGame202504/Assets/Scripts/PlayerController.cs
+++ JamGame202504/Assets/Scripts/PlayerController.cs
@@ -5,21 +5,33 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float moveSpeed = 1.0f;
     [SerializeField] private Animator animator;
     [SerializeField] private Rigidbody rigidBody;
-    
+    [SerializeField] private Transform cameraTransform;
+
     private void FixedUpdate()
     {
         // 入力の取得
         var moveHorizontal = Input.GetAxis("Horizontal");
         var moveVertical = Input.GetAxis("Vertical");
-        var movement = new Vector3(moveHorizontal, 0.0f, moveVertical).normalized;
 
-        // 移動
+        // カメラの向きに基づく方向ベクトル
+        var camForward = cameraTransform.forward;
+        var camRight = cameraTransform.right;
+
+        camForward.y = 0f;
+        camRight.y = 0f;
+
+        camForward.Normalize();
+        camRight.Normalize();
+
+        var movement = (camForward * moveVertical + camRight * moveHorizontal).normalized;
+
+        // 移動処理
         rigidBody.MovePosition(rigidBody.position + movement * (moveSpeed * Time.fixedDeltaTime));
 
         // アニメーショの制御
         var isMove = movement.magnitude > 0;
-        animator.SetBool("isRunning", isMove); 
-        
+        animator.SetBool("isRunning", isMove);
+
         if (isMove)
         {
             // 移動方向にキャラクターを回転させる
